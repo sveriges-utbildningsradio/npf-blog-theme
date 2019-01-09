d9dbdcd5f88c56094bcc3a78ae8425056438e1a5
Joakim Forslund <joakim.forslund@hotmail.com>
2019-01-09 12:43:43 +0100

    New post type, Inspiration. To easily seperate regular posts from Inspirational posts that dont show on the firstpage. New page template to manage different types of Inspirational posts.

--- lib/extras.php
+++ lib/extras.php
@@ -31,3 +31,81 @@ function excerpt_more() {
   return '&hellip;';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+// Register post type "Diverse"
+function cpt_init() {
+
+    $labels = array(
+      'name'               => __( 'Inspiration' ), // post type general name
+      'singular_name'      => __( 'Inspiration' ), // post type singular name
+      'menu_name'          => __( 'Inspiration' ), // admin menu
+      'name_admin_bar'     => __( 'Inspiration' ), // add new on admin bar
+      'add_new'            => __( 'Add New' ), // 'book'
+      'add_new_item'       => __( 'Add New inspiration' ),
+      'new_item'           => __( 'New Inspiration' ),
+      'edit_item'          => __( 'Edit Inspiration' ),
+      'view_item'          => __( 'View Inspiration' ),
+      'all_items'          => __( 'All Inspirations' ),
+      'search_items'       => __( 'Search Inspirations' ),
+      'parent_item_colon'  => __( 'Parent Inspirations:' ),
+      'not_found'          => __( 'No Inspirations found.' ),
+      'not_found_in_trash' => __( 'No Inspirations found in Trash.' )  
+    );
+
+    $args = array(
+      'labels'  => $labels,
+      'public'             => true,
+      'publicly_queryable' => true,
+      'show_ui'            => true,
+      'show_in_menu'       => true,
+      'query_var'          => true,
+      'rewrite'            => array( 'slug' => 'inspiration' ),
+      'capability_type'    => 'post',
+      'has_archive'        => true,
+      'hierarchical'       => false,
+      'menu_position'      => 5,
+      'taxonomies'         => array('section', 'post_tag'),
+      'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt')
+    );
+    register_post_type( 'inspiration', $args );
+}
+add_action( 'init', __NAMESPACE__ . '\\cpt_init' );
+
+
+// Register custom taxonomy
+function taxonomy_init(){
+
+  $labels = array(
+    'name'                       => _x( 'Sections', 'taxonomy general name', 'textdomain' ),
+    'singular_name'              => _x( 'Section', 'taxonomy singular name', 'textdomain' ),
+    'search_items'               => __( 'Search Sections', 'textdomain' ),
+    'popular_items'              => __( 'Popular Sections', 'textdomain' ),
+    'all_items'                  => __( 'All Sections', 'textdomain' ),
+    'parent_item'                => null,
+    'parent_item_colon'          => null,
+    'edit_item'                  => __( 'Edit Section', 'textdomain' ),
+    'update_item'                => __( 'Update Section', 'textdomain' ),
+    'add_new_item'               => __( 'Add New Section', 'textdomain' ),
+    'new_item_name'              => __( 'New Section Name', 'textdomain' ),
+    'separate_items_with_commas' => __( 'Separate Sections with commas', 'textdomain' ),
+    'add_or_remove_items'        => __( 'Add or remove Sections', 'textdomain' ),
+    'choose_from_most_used'      => __( 'Choose from the most used Sections', 'textdomain' ),
+    'not_found'                  => __( 'No Sections found.', 'textdomain' ),
+    'menu_name'                  => __( 'Sections', 'textdomain' ),
+  );
+
+  $args = array(
+    'hierarchical'          => true,
+    'labels'                => $labels,
+    'show_ui'               => true,
+    'show_admin_column'     => true,
+    'update_count_callback' => '_update_post_term_count',
+    'query_var'             => true,
+    'rewrite'               => array( 'slug' => 'section' ),
+  );
+
+  register_taxonomy( 'section', 'inspiration', $args );
+}
+add_action( 'init', __NAMESPACE__ . '\\taxonomy_init' );
+
+
--- template-inspiration.php
+++ template-inspiration.php
@@ -3,108 +3,117 @@
  * Template Name: Inspiration
  */
 ?>
+
+<?php
+
+$insp_args = array(
+	'posts_per_page' => -1,
+	'order' => 'DESC', 
+	'orderby' => 'date',
+	'tax_query' => array(
+		array(
+			'taxonomy' => 'section',
+			'field' => 'slug',
+			'terms' => get_field('inspiration_category')->name
+		)
+	)
+);
+
+$insp_query = new WP_Query($insp_args);
+
+?>
+
+
 <div class="container">
-	<div class="row justify-content-center">
-		<div class="col-12 col-md-10">
-			<?php while (have_posts()) : the_post(); ?>
+	<div class="row">
 
-				<?php $postsImg = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' ); ?>
-				<?php $file = get_field('video_fil') ?: get_field('video_url'); ?>
+	<?php while (have_posts()) : the_post(); ?>
+		<div class="col-12">
+			<h1><?php the_title(); ?></h1>
+			<?php the_content(); ?>
+		</div>
+	<?php endwhile; ?>
+		<?php if ( $insp_query->have_posts() ) { ?>
+			<?php 
+			$counter = 0;
+			while ( $insp_query->have_posts() ) { $insp_query->the_post(); ?>
 
-				<?php if( $file && $postsImg ): ?>
-					<?php //The title for image alt/aria attribute ?>
-					<?php $title = get_post(get_post_thumbnail_id())->post_title;  ?>
+				<?php if ($counter % 4 == 0): ?>
+					<?php 
+					$postID = $post->ID;
+					$postImg = wp_get_attachment_image_src( get_post_thumbnail_id($postID), 'medium' );
+					$embed = get_field('video_url', $postID); ?>
+						<div class="col-sm-12 col-md-12">
+							<a href="<?= get_permalink($postObj->ID); ?>" class="layer-effect">
+								<div class="two-columns item">
+									<div class="row">
+										<div class="col-md-6">
+										<?php if($postImg): ?>
 
-					<?php if( is_string($file)) : ?>
-                        <div class="video-embed-container">
-                            <iframe title="<?php the_title(); ?>"
-                                    src="<?php echo $file ?>"
-                                    width="100%"
-                                    frameborder="0"
-                                    allowfullscreen>
-                            </iframe>
-                        </div>
-						<?php else : ?>
-                        <div class="video-element-container">
-                            <video data-url="<?php the_permalink(); ?>" id="click" preload="none" controlsList="nodownload" poster="<?php echo $postsImg[0];?>" controls>
-                                <source src="<?php echo $file['url']; ?>" type="video/mp4">
-                                Din webbläsare stödjer inte HTML5 video!
-                            </video>
-                            <img class="custom-play play" alt="Play button" src="<?php echo get_template_directory_uri(); ?>/dist/images/Play.svg">
-                            <img class="custom-play pause" alt="Pause button" src="<?php echo get_template_directory_uri(); ?>/dist/images/Paus.svg">
-                        </div>
-					<?php endif; ?>
-				<?php elseif( $postsImg ) : ?>
-					<?php //The title for image alt/aria attribute ?>
-					<?php $title = get_post(get_post_thumbnail_id())->post_title;  ?>
-
-					<div class="post-img" style="background-image: url('<?php echo $postsImg[0];?>');" role="img" alt="<?php echo $title ?>" aria-label="<?php echo $title ?>"></div>
-				<?php endif; ?>
-
-				<div class="content">
-					<h1><?php the_title(); ?></h1>
-					<?php the_content(); ?>
-				</div> <!-- post-content -->
-
-				<div class="time-share">
-					<!-- Date -->
-					<div class="time"></div>
-					<!-- Share -->
-					<div class="share"><i class="fa fa-share-alt"></i> Dela</div>
-					<div class="so-me">
-						<?php $permalink = get_permalink(); ?>
-						<?php $date = date('Ymd'); ?>
-						<!-- Facebook -->
-						<?php echo do_shortcode( '[addtoany buttons="facebook" url="'. $permalink .'?cmpid=del:fb:'. $date .':foraldrar"]' ); ?>
-						<!-- Facebook Messenger -->
-						<?php echo do_shortcode( '[addtoany buttons="facebook_messenger" url="'. $permalink .'?cmpid=del:ms:'. $date .':foraldrar"]' ); ?>
-						<!-- Whats App -->
-						<?php echo do_shortcode( '[addtoany buttons="whatsapp" url="'. $permalink .'?cmpid=del:wa:'. $date .':foraldrar"]' ); ?>
-						<!-- Email -->
-						<?php echo do_shortcode( '[addtoany buttons="email" url="'. $permalink .'?cmpid=del:em:'. $date .':foraldrar"]' ); ?>
-
-						<input id="copy-url" aria-label="Kopiera delbar länk" type="url" value="<?php echo $permalink ?>?cmpid=del:cl:<?php echo $date ?>:foraldrar" readonly="">
-						<div id="copy-link"><i class="fa fa-link"></i></div>
-						<p id="copy-txt"></p>
-					</div>
-				</div>
-
-			<?php endwhile; wp_reset_postdata(); ?>
-
-			<?php $inspo = new WP_Query( array('post_type' => 'post', 'posts_per_page' => 3, 'cat' => 25)); ?>
-			<div class="related-contaier">
-				<div class="row">
-					<?php while( $inspo->have_posts() ) : $inspo->the_post() ; ?>
-						<div class="col-12 col-md-4">
-							<a class="layer-effect" aria-label="UR Föräldrar inlägg" href="<?php the_permalink(); ?>">
-								<?php $postsImg = wp_get_attachment_image_src( get_post_thumbnail_id(), 'medium' ); ?>
-
-								<?php if($postsImg): ?>
-									<?php //The title for image alt/aria attribute ?>
-									<?php $title = get_post(get_post_thumbnail_id())->post_title;  ?>
-
-									<div class="layer-container">
-										<?php $file = get_field('video_fil'); ?>
-										<?php $embed = get_field('video_url'); ?>
-										<div class="post-img" style="background-image: url('<?php echo $postsImg[0];?>');" role="img" alt="<?php echo $title ?>" aria-label="<?php echo $title ?>">
-											<?php if( $file || $embed ): ?>
-												<div class="video-icon">
-													<img src="<?php echo get_template_directory_uri(); ?>/dist/images/video.svg" alt="Video icon">
-												</div>
-											<?php endif; ?>
+										<div class="layer-container">
+											<div class="post-img" style="background-image: url('<?= $postImg[0]; ?>')" role="img" alt="<?= $postObj->post_title; ?>" aria-label="<?= $postObj->post_title; ?>">
+												<?php if( $embed ): ?>
+													<div class="video-icon">
+														<img src="<?php echo get_template_directory_uri(); ?>/dist/images/video.svg" alt="Video icon">
+													</div>
+												<?php endif; ?>
+											</div>
+											<div class="layer"></div>
 										</div>
-										<div class="layer"></div> <!-- layer -->
-									</div> <!-- layer-container -->
+										<?php endif; ?>
+										</div>
+
+										<div class="col-md-6 no-padding-left">
+										    <div class="post-content">
+										    	<?php echo $postObj; ?>
+												<h4><?= get_the_title($postID); ?></h4>
+												<p><?= get_the_excerpt($postID);; ?></p>
+											</div>
+										</div>
+
+									</div>
+								</div>
+							</a>
+						</div>
+
+				<?php else: ?>
+
+					<?php
+			    	$postID = $post->ID;
+			    	$postImg = wp_get_attachment_image_src( get_post_thumbnail_id($postID), 'medium' );
+					$embed = get_field('video_url', $postID); ?>
+					
+			    	<div class="col-sm-12 col-md-4">
+			    		<a href="<?= get_permalink($postObj->ID); ?>" class="layer-effect">
+				    		<div class="three-columns item">
+				    			<?php if($postImg): ?>
+
+								<div class="layer-container">
+									<div class="post-img" style="background-image: url('<?= $postImg[0]; ?>')" role="img" alt="<?= $postObj->post_title; ?>" aria-label="<?= $postObj->post_title; ?>">
+										<?php if( $embed ): ?>
+											<div class="video-icon">
+												<img src="<?php echo get_template_directory_uri(); ?>/dist/images/video.svg" alt="Video icon">
+											</div>
+										<?php endif; ?>
+									</div>
+									<div class="layer"></div>
+								</div>
 								<?php endif; ?>
 
-								<div class="post-content">
-									<h5><?php the_title(); ?></h5>
-								</div> <!-- post-content -->
-							</a> <!-- layer-effect -->
-						</div> <!-- col-12 col-md-4 -->
-					<?php endwhile; wp_reset_postdata(); ?>
-				</div> <!-- row -->
-			</div> <!-- py-4 -->
-		</div> <!-- col-12 col-md-10 -->
-	</div> <!-- row justify-content-center -->
+							    <div class="post-content">
+									<h4><?= get_the_title($postID); ?></h4>
+									<p><?= get_the_excerpt($postID);; ?></p>
+								</div>
+
+							</div>
+						</a>
+					</div>
+
+				<?php endif ?>
+				<?php
+				$counter++;
+			}
+			wp_reset_postdata();
+		} ?>
+	</div> <!-- row -->
 </div> <!-- container -->
